c72e9b56817461622bcc649c801cef35982beec5
jerome132 <86404985+jerome132@users.noreply.github.com>
2022-09-21 11:32:51 +0900

    Add files via upload

---
+++ 05_example_2/05_example_2.ino
@@ -0,0 +1,28 @@
+#define PIN_LED 7
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) {
+    ; //wait for serial port to connect.
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(PIN_LED, 0);
+  delay(1000);
+  digitalWrite(PIN_LED, 1);
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(PIN_LED, 0);
+    delay(100);
+    digitalWrite(PIN_LED, 1);
+    delay(100);
+  }
+
+  while(1) {
+
+  } 
+}
+
